d25dfb342e647ae029ead21c7605812fca27bb2a
Ouronade <121133292+Ouronade@users.noreply.github.com>
2023-01-30 11:53:05 +0100

    Add files via upload

---
+++ TP2-03_bob.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+    unsigned long gros_nombre;
+
+//Le type lui permettant d'avoir le plus gros nombre entier non négatif est unsigned long
+
+    printf("Entrez un gros nombre : ");
+    scanf("%ld", &gros_nombre);
+    printf("%ld, un gros nombre ?\n", gros_nombre);
+
+    gros_nombre = 999999999999999999;
+
+    printf("%X, un gros nombre !\n", gros_nombre);
+
+    exit(EXIT_SUCCESS);
+}
+
+//int ne prends que 4 octects, ce qui limite donc sa valeur, long quand à lui peut prendre jusqu'à 8 octets.
