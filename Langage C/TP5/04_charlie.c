52f1baacea2dea21bc983e46ac9f37d570617dda
Ouronade <121133292+Ouronade@users.noreply.github.com>
2023-03-20 12:18:22 +0100

    Create 04_charlie.c

---
+++ Langage C/TP5/04_charlie.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+    int a;
+    int b;
+    int pgcd;
+    
+    printf("Entrez deux entiers : ");
+    scanf("%d %d", &a, &b);
+
+    exit(EXIT_SUCCESS);
+}
